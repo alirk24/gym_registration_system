63f9ec34aad7cf1a1ab4a3982c7ee5b310767a54
alirk24 <ali.rk24@gmail.com>
2024-12-14 23:00:24 +0330

    membership plan added to admin panel

--- gym_registration/members/admin.py
+++ gym_registration/members/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from jalali_date.admin import ModelAdminJalaliMixin
-from .models import Member
+from .models import Member, MembershipPlan
 
 class MemberAdmin(ModelAdminJalaliMixin, admin.ModelAdmin):
     list_display = ('name', 'LastName', 'registration_date_jalali', 'remaining_days')
@@ -8,3 +8,4 @@ class MemberAdmin(ModelAdminJalaliMixin, admin.ModelAdmin):
     fields = ('name', 'LastName', 'phone', 'gender', 'date_of_birth', 'registration_date', 'MembershipPlan')  # Fields to display/edit
 
 admin.site.register(Member, MemberAdmin)
+admin.site.register(MembershipPlan)
